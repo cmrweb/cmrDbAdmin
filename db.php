68e373d4a3ff3b253107a47c3430f5a06c81e6a6
tykrasta <tykrastagames@gmail.com>
2019-02-24 10:31:01 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,24 @@
 
 <?php
+//variable
 $msg=$msg2=$msg3=$msg4=$tblname=$dbname=$servername=$username=$password="";
-
+//connexion pour creation de base de donnée
+function fconnect($servername, $username, $password){
+    $GLOBALS['conn'] = new mysqli($servername, $username, $password);
+    if ($GLOBALS['conn']->connect_error) {
+        die("creation failed: " . $GLOBALS['conn']->connect_error);
+    }
+}
+//connexion
+function connect($servername, $username, $password,$dbname){
+    $GLOBALS['conn']=new mysqli($servername, $username, $password, 'cmr_'.$dbname);
+    if ($GLOBALS['conn']->connect_error) {
+        die("Connection failed: " . $GLOBALS['conn']->connect_error);
+    }
+}
+/*
+*   Base de donée
+*/
 if(isset($_GET['create'])){ 
     $servername = $_GET['dbserv'];
     $username = $_GET['dbuser'];
@@ -9,63 +26,61 @@ if(isset($_GET['create'])){
     $dbname=$_GET['dbname']; 
     
     if(!empty($dbname)){
-        $conn = new mysqli($servername, $username, $password);
-        if ($conn->connect_error) {
-            die("creation failed: " . $conn->connect_error);
-        }
+        fconnect($servername, $username, $password);
         $req="CREATE DATABASE IF NOT EXISTS cmr_$dbname CHARACTER SET utf8 COLLATE utf8_general_ci";
         mysqli_query($conn,$req);
-        $msg= 'base de donnée crée';
-        
-        $conn=new mysqli($servername, $username, $password, 'cmr_'.$dbname);
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
+        $msg= 'base de donnée crée';     
+        connect($servername, $username, $password,$dbname);
         $msg.= ' et connecter : cmr_'.$dbname;
-    } 
-
+    }else{
+        $msg= 'entrer un nom de base de donnée';
+    }
 }
+/*
+*   Table
+*/
 if(isset($_POST['createtble'])){
      $tblname=$_POST['tblename'];
     if(!empty($tblname)&& !empty($_POST['champ'])){
-      $req= 'CREATE TABLE '.$tblname.' ('; 
-    foreach ($_POST['champ'] as $field) {
-        if(!empty($field)){
-            $req.=$field;
-        }     
+        $req= 'CREATE TABLE '.$tblname.' ('; 
+        foreach ($_POST['champ'] as $field) {
+            if(!empty($field)){
+                $req.=$field;
+            }     
     }
     $req.= ')';
     $msg2= 'Envoyer : '.$req;
-    $conn=new mysqli($servername, $username, $password, 'cmr_'.$dbname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+    connect($servername, $username, $password,$dbname);
     mysqli_query($conn,$req);  
+    }else{
+        $msg2= 'Veuillez remplir les champs';
     }
-   
-    
-
-} 
-if(isset($_POST['show'])){
-    $sel=$_POST['sel'];
-    $tbl=$_POST['tbl'];
-    $conn=new mysqli($servername, $username, $password, 'cmr_'.$dbname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    $req2="SELECT $sel FROM $tbl";
-//execute (connexion,request)
-$result = mysqli_query($conn,$req2);
-
-    
 }
+/*
+*   Requete
+*/
 if(isset($_POST['request'])){
     $req=$_POST['keyword'];
-    $msg3= 'Envoyer : '.$req;
-    $conn=new mysqli($servername, $username, $password, 'cmr_'.$dbname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    mysqli_query($conn,$req);
+    if(!empty($req)){
+        $msg3= 'Envoyer : '.$req;
+        connect($servername, $username, $password,$dbname);
+        mysqli_query($conn,$req);   
+    }else{
+        $msg3='Veuillez insérer une requete';
+    }   
+} 
+/*
+*   SELECT - FROM
+*/
+if(isset($_POST['show'])){
+        $sel=$_POST['sel'];
+        $tbl=$_POST['tbl'];
+    if(!empty($sel)&& !empty($tbl)){
+        connect($servername, $username, $password,$dbname);
+        $req2="SELECT $sel FROM $tbl";
+        $result = mysqli_query($conn,$req2);  
+    }else{
+        $msg3= 'Veuillez remplir les champs';
+    }  
 }
 ?>
